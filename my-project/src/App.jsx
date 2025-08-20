2622be99efe72b1efd9bb34b324e61db3f724753
Qoz156 <qozfahussain@gmail.com>
2025-08-20 17:12:12 +0100

    Set up React project and installed tailwind

---
+++ my-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <>
+    <div class="bg-green-100 h-15 w-full border-2 rounded-md my-10 p-6 flex justify-center items-center">
+      <h1 class="text-center font-mono font-extrabold text-red-400 mt-4">Hello world!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App;
+
