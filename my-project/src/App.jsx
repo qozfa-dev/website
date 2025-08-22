8fdb7a194d3558c86be256b6d094118019127150
Qoz156 <qozfahussain@gmail.com>
2025-08-22 17:28:20 +0100

    Created Home page and added name and tagline with styling

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import './pages/Home.jsx';
+import Home from './pages/Home.jsx';
 
 export const App = () => {
   return (
     <>
-    <div class="bg-green-100 h-15 w-full border-2 rounded-md my-10 p-6 flex justify-center items-center">
-      <h1 class="text-center font-mono font-extrabold text-red-400 mt-4">Hello world!</h1>
-    </div>
+    <Home />
     </>
   )
 }
---
+++ my-project/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div class="flex items-center justify-center h-full text-center">
+        <div class="flex flex-col items-center mt-30">
+        <h1 class="text-[#A78BFA] text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-sans font-bold mt-[10vh] mx-1">Qozfa Hussain</h1>
+        <h2 class="text-[#E5E7EB] pt-5 mt-3.5 md:text-2xl lg:text-2.1x1">2nd Year CS Student at King’s College London</h2>
+        <h2 class="text-[#E5E7EB] md:text-2xl lg:text-2.1x1">STEMM '25 Hackathon Winner</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Home;
