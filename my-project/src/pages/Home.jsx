2d5db214953b5bccd43ab029ed086c0ec83480fb
Qoz156 <qozfahussain@gmail.com>
2025-08-22 17:43:17 +0100

    Created About Me and Projects buttons

--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -7,6 +7,10 @@ const Home = () => {
         <h1 class="text-[#A78BFA] text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-sans font-bold mt-[10vh] mx-1">Qozfa Hussain</h1>
         <h2 class="text-[#E5E7EB] pt-5 mt-3.5 md:text-2xl lg:text-2.1x1">2nd Year CS Student at King’s College London</h2>
         <h2 class="text-[#E5E7EB] md:text-2xl lg:text-2.1x1">STEMM '25 Hackathon Winner</h2>
+        <div class="flex flex-row mt-7 justify-center">
+        <button type="button" class="bg-[#A78BFA] text-white border-none mr-5 m-2 px-2 py-2.5 rounded-2xl">About Me</button>
+        <button type="button" class="bg-[#A78BFA] text-white border-none ml-5 m-2 px-2 py-2.5 rounded-2xl">Projects</button>
+        </div>
         </div>
     </div>
   )
