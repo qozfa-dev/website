4dfec6c7a1a12cc647c66a4dd5b33fd4e3756e96
Qoz156 <qozfahussain@gmail.com>
2025-08-23 16:20:16 +0100

    Added section id and fixed margin issues in Home section

--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -3,10 +3,10 @@ import '../components/Navbar.jsx';
 
 const Home = () => {
   return (
-    <>
+    <section id="Home" className="h-screen">
     <div class="flex items-center justify-center h-full text-center">
-        <div class="flex flex-col items-center mt-20">
-        <h1 class="text-[#A78BFA] text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-sans font-bold mt-[10vh] mx-1">Qozfa Hussain</h1>
+        <div class="flex flex-col items-center">
+        <h1 class="text-[#A78BFA] text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-sans font-bold">Qozfa Hussain</h1>
         <h2 class="text-[#E5E7EB] pt-5 mt-3.5 md:text-2xl lg:text-2.1x1">2nd Year CS Student at King’s College London</h2>
         <h2 class="text-[#E5E7EB] md:text-2xl lg:text-2.1x1">STEMM '25 Hackathon Winner</h2>
         <div class="flex flex-row mt-7 justify-center w-full">
@@ -15,7 +15,7 @@ const Home = () => {
         </div>
         </div>
     </div>
-    </>
+    </section>
   )
 }
 
