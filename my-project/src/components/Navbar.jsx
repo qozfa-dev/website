cec1a7037055c668958fd0785169db2db2453faa
Qoz156 <qozfahussain@gmail.com>
2025-08-23 13:41:14 +0100

    Created navbar

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div class="text-[#FFFFFF] bg-[#0D1117] flex sticky justify-end items-center p-3 m-2">
+    <ul>
+      <li><a href="#Home">Home</a></li>
+      <li><a href="#About">About Me</a></li>
+      <li><a href="#Projects">Projects</a></li>
+      <li><a href="#Contact">Contact</a></li>
+    </ul>
+    </div>
   )
 }
 
