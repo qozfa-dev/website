101a0462183adee81f9391ff91e728c4f7b07762
Qoz156 <qozfahussain@gmail.com>
2025-08-24 22:10:22 +0100

    Created all project cards with same styling

--- my-project/src/pages/Projects.jsx
+++ my-project/src/pages/Projects.jsx
@@ -5,13 +5,39 @@ const Projects = () => {
     <section id="Projects" className="h-screen">
       <div className="px-10 py-5">
       <h2 className="text-[#A78BFA] text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-sans font-bold mt-5 pt-15">Projects</h2>
-      <div className="grid-rows-3 flex justify-between items-center mt-25">
-        <div className="bg-[#1F2937] max-w-44 justify-center items-center rounded-2xl cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out m-5">
+      <div className="grid-rows-3 flex justify-between items-center mt-20">
+
+        <div className="bg-[#1F2937] max-w-50 min-h-65 justify-center items-center rounded-2xl cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out m-5">
           <h2 className="text-[#A78BFA] text-center p-2 font-bold text-[20px]">Portfolio site</h2>
           <p className="text-[#E5E7EB] text-center p-4">This full-stack portfolio site showcases my personal projects with interactive components and resposivity</p>
+          <div className="flex flex-row justify-center space-x-3 p-4 text-[#FFFFFF] text-[14px]">
+            <p>React</p>
+            <p>Tailwind</p>
+            <p>Supabase</p>
+          </div>
         </div>
-        <div className="bg-red-400 max-w-40">div 2</div>
-        <div className="bg-red-800 max-w-40">div 3</div>
+
+        <div className="bg-[#1F2937] max-w-50 min-h-65 justify-center items-center rounded-2xl cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out m-5">
+          <h2 className="text-[#A78BFA] text-center p-2 font-bold text-[20px]">Air Pollution Visualizer</h2>
+          <p className="text-[#E5E7EB] text-center p-4">Interactive JavaFX app visualizing multi-year air quality data with API integration and dynamic charts</p>
+          <div className="flex flex-row justify-center space-x-3 p-4 text-[#FFFFFF] text-[14px]">
+            <p>Java</p>
+            <p>JavaFX</p>
+            <p>API</p>
+          </div>
+        </div>
+
+        <div className="bg-[#1F2937] max-w-50 min-h-65 justify-center items-center rounded-2xl cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out m-5">
+          <h2 className="text-[#A78BFA] text-center p-2 font-bold text-[20px]">Cartpole Game Agent</h2>
+          <p className="text-[#E5E7EB] text-center p-4">A reinforcement learning agent built in PyTorch to solve CartPole-v1, demonstrating AI decision-making</p>
+          <div className="flex flex-row justify-center space-x-3 p-4 text-[#FFFFFF] text-[14px]">
+            <p>Python</p>
+            <p>PyTorch</p>
+            <p>Matplotlib</p>
+          </div>
+        </div>
+
+        
       </div>
       </div>
     </section>
