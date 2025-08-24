ce0262c0a86b9bacf1a839c4f3aeddb4e429c6af
Qoz156 <qozfahussain@gmail.com>
2025-08-24 12:30:26 +0100

    Created Experience page and added padding to Experience and Projects titles

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,6 +3,8 @@ import './pages/Home.jsx';
 import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
+import Projects from './pages/Projects.jsx';
+import Experience from './pages/Experience.jsx';
 
 export const App = () => {
   return (
@@ -10,6 +12,8 @@ export const App = () => {
     <Navbar />
     <Home />
     <About />
+    <Experience />
+    <Projects />
     </div>
   )
 }
---
+++ my-project/src/pages/Experience.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <section id="Experience" className="h-screen">
+        <div className="px-10 py-5">
+        <h2 className="text-[#A78BFA] text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-sans font-bold mt-5 pt-15">Experience</h2>
+        </div>
+    </section>
+  )
+}
+
+export default Experience;  
