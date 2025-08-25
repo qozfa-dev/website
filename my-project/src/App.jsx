37ba5c315950fb22f6b227c48825611a29a36622
Qoz156 <qozfahussain@gmail.com>
2025-08-25 12:26:00 +0100

    Created Contact page

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Projects from './pages/Projects.jsx';
+import Contact from './pages/Contact.jsx';
 
 export const App = () => {
   return (
@@ -12,6 +13,7 @@ export const App = () => {
     <Home />
     <About />
     <Projects />
+    <Contact />
     </div>
   )
 }
