fbe2d4ac5304354dc62ca755cdd9f8f04ee7df91
Qoz156 <qozfahussain@gmail.com>
2025-08-25 14:25:07 +0100

    Made basic skeleton of Contact page

--- my-project/src/pages/Contact.jsx
+++ my-project/src/pages/Contact.jsx
@@ -5,6 +5,12 @@ const Contact = () => {
     <section id="Contact" className="h-screen">
     <div className="px-10 py-5">
         <h2 className="text-[#A78BFA] text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-sans font-bold mt-5 pt-15">Contact</h2>
+        <p className="text-[#E5E7EB] pt-15 text-2xl">Feel free to connect with me!</p>
+        <div className=" space-y-4 mt-15">
+            <li className="text-white list-none"><a href="https://www.linkedin.com/in/qozfa-h/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+            <li className="text-white list-none"><a href="https://github.com/qozfa-dev" target="_blank" rel="noopener noreferrer">Github</a></li>
+            <li className="text-white list-none"><a href="https://www.linkedin.com/in/qozfa-h/" target="_blank" rel="noopener noreferrer">Email</a></li>
+        </div>
     </div>
     </section>
   )
