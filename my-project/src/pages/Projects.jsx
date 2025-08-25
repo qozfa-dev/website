b7fb786c885ce52c8b9e656ce1fd63b34e8b76f8
Qoz156 <qozfahussain@gmail.com>
2025-08-25 12:21:34 +0100

    Made project cards flex grow and responsive columns

--- my-project/src/pages/Projects.jsx
+++ my-project/src/pages/Projects.jsx
@@ -5,11 +5,11 @@ const Projects = () => {
     <section id="Projects" className="h-screen">
       <div className="px-10 py-5">
       <h2 className="text-[#A78BFA] text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-sans font-bold mt-5 pt-15">Projects</h2>
-      <div className="grid-rows-3 flex justify-between items-center mt-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-20">
 
-        <div className="bg-[#1F2937] max-w-50 min-h-65 justify-center items-center rounded-2xl cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out m-5">
+        <div className="bg-[#1F2937] justify-center items-center rounded-2xl cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out m-5 flex flex-col">
           <h2 className="text-[#A78BFA] text-center p-2 font-bold text-[20px]">Portfolio site</h2>
-          <p className="text-[#E5E7EB] text-center p-4">This full-stack portfolio site showcases my personal projects with interactive components and resposivity</p>
+          <p className="text-[#E5E7EB] text-center p-4">This full-stack portfolio site showcases my personal projects with interactive components and responsivity</p>
           <div className="flex flex-row justify-center space-x-3 p-4 text-[#FFFFFF] text-[14px]">
             <p>React</p>
             <p>Tailwind</p>
@@ -17,9 +17,9 @@ const Projects = () => {
           </div>
         </div>
 
-        <div className="bg-[#1F2937] max-w-50 min-h-65 justify-center items-center rounded-2xl cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out m-5">
+        <div className="bg-[#1F2937] justify-center items-center rounded-2xl cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out m-5 flex flex-col">
           <h2 className="text-[#A78BFA] text-center p-2 font-bold text-[20px]">Air Pollution Visualizer</h2>
-          <p className="text-[#E5E7EB] text-center p-4">Interactive JavaFX app visualizing multi-year air quality data with API integration and dynamic charts</p>
+          <p className="text-[#E5E7EB] text-center p-4">Interactive JavaFX app visualizing multi-year air quality data with API integration and dynamic charts. </p>
           <div className="flex flex-row justify-center space-x-3 p-4 text-[#FFFFFF] text-[14px]">
             <p>Java</p>
             <p>JavaFX</p>
@@ -27,7 +27,7 @@ const Projects = () => {
           </div>
         </div>
 
-        <div className="bg-[#1F2937] max-w-50 min-h-65 justify-center items-center rounded-2xl cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out m-5">
+        <div className="bg-[#1F2937] justify-center items-center rounded-2xl cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out m-5 flex flex-col">
           <h2 className="text-[#A78BFA] text-center p-2 font-bold text-[20px]">Cartpole Game Agent</h2>
           <p className="text-[#E5E7EB] text-center p-4">A reinforcement learning agent built in PyTorch to solve CartPole-v1, demonstrating AI decision-making</p>
           <div className="flex flex-row justify-center space-x-3 p-4 text-[#FFFFFF] text-[14px]">
