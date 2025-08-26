b50775b8d26d4b0a0aa9c26b1c1bcbe97036699a
Qoz156 <qozfahussain@gmail.com>
2025-08-26 16:46:47 +0100

    Changed Contact page styling, added responsive CTA

--- my-project/src/pages/Contact.jsx
+++ my-project/src/pages/Contact.jsx
@@ -5,11 +5,10 @@ const Contact = () => {
     <section id="Contact" className="min-h-screen pt-10">
     <div className="px-10 py-5">
         <h2 className="text-[#A78BFA] text-3xl sm:text-4xl md:text-5xl lg:text-5xl font-sans font-bold mt-5 pt-15">Contact</h2>
-        <p className="text-[#E5E7EB] pt-15 text-2xl">Feel free to connect with me!</p>
-        <div className=" space-y-4 mt-15">
-            <li className="text-white list-none"><a href="https://www.linkedin.com/in/qozfa-h/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-            <li className="text-white list-none"><a href="https://github.com/qozfa-dev" target="_blank" rel="noopener noreferrer">Github</a></li>
-            <li className="text-white list-none"><a href="https://www.linkedin.com/in/qozfa-h/" target="_blank" rel="noopener noreferrer">Email</a></li>
+        
+        <div className="space-y-4 flex flex-col justify-center items-center mt-10">
+          <p className="mt-15 text-[#E5E7EB] sm:text-1.5xl md:text-2xl lg:text-2.5xl">Email me to get in touch!</p>
+            <button className=" flex justify-center container  flex-1 max-w-[60px] sm:max-w-[90px] xs:max-w-[70px] md:max-w-[120px] lg:max-w-[160px] text-xs sm:text-sm md:text-base lg:text-lg bg-[#A78BFA] text-white border-none mr-5 m-2 px-2 py-2.5 rounded-2xl hover:bg-purple-700 active:bg-[#A78BFA] cursor-pointer">Email Me</button>
         </div>
     </div>
     </section>
--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div class="flex items-center justify-center h-full text-center">
         <div className="flex flex-col items-center">
         <h1 className="text-[#A78BFA] text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-sans font-bold">Qozfa Hussain</h1>
-        <h2 className="text-[#E5E7EB] mt-3.5 md:text-2xl lg:text-2.1x1">2nd Year CS Student at King’s College London</h2>
+        <h2 className="text-[#E5E7EB] pt-5 mt-3.5 md:text-2xl lg:text-2.1x1">2nd Year CS Student at King’s College London</h2>
         <h2 className="text-[#E5E7EB] md:text-2xl lg:text-2.1x1">STEMM '25 Hackathon Winner</h2>
         <div className="flex flex-row mt-7 justify-center w-full">
         <button type="button" className="container mx-auto flex-1 max-w-[60px] sm:max-w-[90px] xs:max-w-[70px] md:max-w-[120px] lg:max-w-[160px] text-xs sm:text-sm md:text-base lg:text-lg bg-[#A78BFA] text-white border-none mr-5 m-2 px-2 py-2.5 rounded-2xl hover:bg-purple-700 active:bg-[#A78BFA] cursor-pointer">About Me</button>
