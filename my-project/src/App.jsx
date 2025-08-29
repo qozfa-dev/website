255cc445cb9efce61b4d1646f85ac8cd48df8f2a
Qoz156 <qozfahussain@gmail.com>
2025-08-29 16:25:03 +0100

    Created useEffect toggle logic in App.jsx

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -6,9 +6,22 @@ import Projects from './pages/Projects.jsx';
 import Contact from './pages/Contact.jsx';
 
 export const App = () => {
+  const [theme, setTheme] = useState('dark');
+  
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+      localStorage.theme = "dark";
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = "light";
+    }
+  }, [theme]);
+
+
   return (
-    <div className="scroll-smooth bg-[#FFF8F0] dark:bg-[##111827]">
-    <Navbar />
+    <div className="scroll-smooth bg-[#FFF8F0] dark:bg-[#111827] transition-colors">
+    <Navbar theme={theme} setTheme={setTheme}/>
     <Home />
     <About />
     <Projects />
